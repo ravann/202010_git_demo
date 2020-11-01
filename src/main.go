5c11bb5173710eebf60c34b01e6ca141836b710a
SreenivasaRao Chennupati <sreenivasaraochennupati@abhi-air.lan>
2020-11-01 11:57:14 +0800

    Include Server Update

--- src/main.go
+++ src/main.go
@@ -3,7 +3,8 @@ package main
 import (
     "net/http"
     "log"
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+    "time"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +16,13 @@ func main() {
     // Routes consist of a path and a handler function.
     r.HandleFunc("/", HelloWorld)
 
+    server := &http.Server{
+        Addr:           ":8000",
+        Handler:        r,
+        ReadTimeout:    5 * time.Second,
+        WriteTimeout:   5 * time.Second,
+    }
+
     // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8000", r))
+    log.Fatal(server.ListenAndServe())
 }
